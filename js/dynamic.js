f8880079964fda8b4a062f078726a2ab53409043
Yudai Shinnoki <y.shinnoki@gmail.com>
2018-05-28 08:41:44 +0900

    change camera motion

--- js/dynamic.js
+++ js/dynamic.js
@@ -9,17 +9,17 @@ AFRAME.registerComponent('dynamic', {
     var maxInterval = 1 / 60 * 1000;
     dt = Math.min(dt, maxInterval);
 
-    var entity = this.el;
+    var el = this.el;
 
     // 消失判定
-    var y = entity.object3D.position.y;
+    var y = el.object3D.position.y;
     if (y < -100) {
-      return entity.parentNode.removeChild(entity);
+      return el.parentNode.removeChild(el);
     }
 
     // 重力の計算
-    var velocity = entity.getAttribute('velocity') || {x: 0, y: 0, z: 0};
-    entity.setAttribute('velocity', {
+    var velocity = el.getAttribute('velocity') || {x: 0, y: 0, z: 0};
+    el.setAttribute('velocity', {
       x: velocity.x,
       y: velocity.y - 9.8 * dt / 1000,
       z: velocity.z
@@ -36,12 +36,12 @@ AFRAME.registerComponent('velocity', {
     var maxInterval = 1 / 60 * 1000;
     dt = Math.min(dt, maxInterval);
 
-    var entity = this.el;
+    var el = this.el;
 
-    var velocity = entity.getAttribute('velocity') || {x: 0, y: 0, z: 0};
-    var position = entity.object3D.position;
+    var velocity = el.getAttribute('velocity') || {x: 0, y: 0, z: 0};
+    var position = el.object3D.position;
 
-    entity.object3D.position.set(
+    el.object3D.position.set(
       position.x + velocity.x * dt / 1000,
       position.y + velocity.y * dt / 1000,
       position.z + velocity.z * dt / 1000
@@ -58,12 +58,12 @@ AFRAME.registerComponent('angular-velocity', {
     var maxInterval = 1 / 60 * 1000;
     dt = Math.min(dt, maxInterval);
 
-    var entity = this.el;
+    var el = this.el;
 
-    var angularVelocity = entity.getAttribute('angular-velocity') || {x: 0, y: 0, z: 0};
-    var rotation = entity.object3D.rotation;
+    var angularVelocity = el.getAttribute('angular-velocity') || {x: 0, y: 0, z: 0};
+    var rotation = el.object3D.rotation;
 
-    entity.object3D.rotation.set(
+    el.object3D.rotation.set(
       rotation.x + angularVelocity.x * dt / 1000,
       rotation.y + angularVelocity.y * dt / 1000,
       rotation.z + angularVelocity.z * dt / 1000
